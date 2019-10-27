7312f5ea18e809ffd6fcd62e451df82d3f85be39
Alexander Giuliano <50060357+AlexGiuliano@users.noreply.github.com>
2019-10-27 19:14:39 -0400

    Add files via upload
    Added attacks and changed how objects are stored by the manager

---
+++ FightServer/WindowsFormsApplication1/world_objects/platform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    public class platform : world_object
+    {
+        public platform(int x, int y, int width, int height, object_manager o)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+
+            manager = o;
+
+            this.clr = System.Drawing.Color.Black;
+        }
+
+
+    }
+}
