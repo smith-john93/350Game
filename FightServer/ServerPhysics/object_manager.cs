d14a2e6cf0b1624c414bac64c9b5583eb0886512
Alexander Giuliano <50060357+AlexGiuliano@users.noreply.github.com>
2019-11-23 12:50:50 -0500

    Update object_manager.cs

--- FightServer/ServerPhysics/object_manager.cs
+++ FightServer/ServerPhysics/object_manager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Collections.Generic;
 
@@ -8,20 +8,26 @@ namespace WindowsFormsApplication1
 {
     public class object_manager
     {
-        public List<world_object> world_object_list;
-        public List<platform> platform_list;// a list of objects that act as platforms
-        public List<player> control_list;// a list of objects that players control
-        public List<attack> attack_list;// a list of attacks
+        public List<List<world_object>> world_object_list;
 
+        public List<world_object> platform_list;// a list of objects that act as platforms
+        public List<world_object> player_list;// a list of objects that players control
+        public List<world_object> attack_list;// a list of attacks
+
+        public int world_object_count = 0;
 
 
         public  object_manager()
         {
+            world_object_list = new List<List<world_object>>();
+
             // Create a list of platforms.
-            world_object_list = new List<world_object>();
-            platform_list = new List<platform>();
-            control_list = new List<player>();
-            attack_list = new List<attack>();
+            platform_list = new List<world_object>();
+            world_object_list.Add(platform_list);
+            player_list = new List<world_object>();
+            world_object_list.Add(player_list);
+            attack_list = new List<world_object>();
+            world_object_list.Add(attack_list);
 
             create_platform(50, 300, 150, 200);
             create_platform(150, 450, 450, 50);
@@ -31,34 +37,55 @@ namespace WindowsFormsApplication1
             create_platform(300, 150, 100, 25);
             create_platform(150, 150, 100, 25);
 
-            player p = create_player(Fighter.normal, 150, 30);
-            p = create_player(Fighter.normal, 500, 30);
+            
+    
+
+            new player(Fighter.lego, this, 150, 30);
+            player p = new player(Fighter.lego, this, 500, 30);
             p.isactive = false;
+
+        }
+
+        public object_manager(System.Net.Sockets.NetworkStream player1_stream,Fighter PlayerOneFighter, System.Net.Sockets.NetworkStream player2_stream, Fighter PlayerTwoFighter)
+        {
+            world_object_list = new List<List<world_object>>();
+
+            // Create a list of platforms.
+            platform_list = new List<world_object>();
+            world_object_list.Add(platform_list);
+            player_list = new List<world_object>();
+            world_object_list.Add(player_list);
+            attack_list = new List<world_object>();
+            world_object_list.Add(attack_list);
+
+            create_platform(50, 300, 150, 200);
+            create_platform(150, 450, 450, 50);
+            create_platform(600, 400, 100, 100);
+            create_platform(700, 100, 50, 400);
+            create_platform(550, 100, 50, 250);
+            create_platform(300, 150, 100, 25);
+            create_platform(150, 150, 100, 25);
+
+
+
+
+            new player(PlayerOneFighter, this, 150, 30, player1_stream);
+            player p = new player(PlayerTwoFighter, this, 500, 30, player2_stream);
+            p.isactive = false;
+
         }
 
         public void create_platform(int x, int y, int width, int height)
         {
             platform p = new platform(x, y, width, height, this);
-
-            world_object_list.Add(p);
-            platform_list.Add(p);
-        }
-
-        public player create_player(Fighter f, int x, int y)
-        {
-            player p = new player(f, this, x, y);
-
-            world_object_list.Add(p);
-            control_list.Add(p);
-
-            return p;
+            
+            
         }
 
         public void create_attack(int x, int y, player owner)
         {
             attack a = new attack(x, y, owner, this);
 
-            world_object_list.Add(a);
             attack_list.Add(a);
         }
 
@@ -66,7 +93,6 @@ namespace WindowsFormsApplication1
         {
             attack a = new projectile(x, y, owner, this);
 
-            world_object_list.Add(a);
             attack_list.Add(a);
         }
 
@@ -81,23 +107,29 @@ namespace WindowsFormsApplication1
         {
             g.Clear(System.Drawing.Color.White);
 
-            for (int i = world_object_list.Count - 1; i >= 0; i--)///must be a for loop because it is changing the list inside of it
+            foreach(List<world_object> wol in world_object_list)
             {
-                world_object_list[i].draw_tick(g);
+                for (int i = wol.Count - 1; i >= 0; i--)///must be a for loop because it is changing the list inside of it
+                {
+                    wol[i].draw_tick(g);
+                }
             }
         }
 
         public void game_tick()
         {
-            for (int i = world_object_list.Count - 1; i >= 0; i--)///must be a for loop because it is changing the list inside of it
+            foreach (List<world_object> wol in world_object_list)
             {
-                world_object_list[i].game_tick();
+                for (int i = wol.Count - 1; i >= 0; i--)///must be a for loop because it is changing the list inside of it
+                {
+                    wol[i].game_tick();
+                }
             }
         }
 
         public void key_down(char c)
         {
-            foreach (player play in control_list)
+            foreach (player play in player_list)
             {
                 play.key_down(c);
             }
@@ -105,7 +137,7 @@ namespace WindowsFormsApplication1
 
         public void key_up(char c)
         {
-            foreach (player play in control_list)
+            foreach (player play in player_list)
             {
                 play.key_up(c);
             }
