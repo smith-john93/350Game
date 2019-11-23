ab90961454e75a35bb62602a8b0e8b5e1cb984f9
Alexander Giuliano <50060357+AlexGiuliano@users.noreply.github.com>
2019-11-23 12:49:31 -0500

    Update Form1.cs

--- FightServer/ServerPhysics/Form1.cs
+++ FightServer/ServerPhysics/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,17 +14,28 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        object_manager obj_man;
+        public System.Net.Sockets.NetworkStream player1_stream;
+        public System.Net.Sockets.NetworkStream player2_stream;
 
-        object_manager obj_man = new object_manager();
+
+        public Form1(System.Net.Sockets.NetworkStream player1_stream, Fighter PlayerOneFighter, System.Net.Sockets.NetworkStream player2_stream, Fighter PlayerTwoFighter)
+        {
+            InitializeComponent();
+
+            obj_man = new object_manager(player1_stream, PlayerOneFighter, player2_stream, PlayerTwoFighter);
+        }
 
         public Form1()
         {
             InitializeComponent();
+
+            obj_man = new object_manager();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -40,7 +51,7 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-
+            timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -61,7 +72,7 @@ namespace WindowsFormsApplication1
         {
             this.KeyPreview = true;
 
-            timer1.Start();
+            
         }
 
 
