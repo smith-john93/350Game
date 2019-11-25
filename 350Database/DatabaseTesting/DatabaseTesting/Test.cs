449181beeccd7354f5bc071b5c51a2b588829a92
Tim Cahill <cahill035@wcsu.edu>
2019-11-25 18:58:17 -0500

    Updated

---
+++ 350Database/DatabaseTesting/DatabaseTesting/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DatabaseTesting
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+
+            // Add Users
+            try
+            {
+                database.AddNewUser("PasswordIsTACO", "TACO");
+            }
+            catch (System.Data.SQLite.SQLiteException e)
+            {
+                Console.WriteLine("Username already exists.");
+            }
+            try
+            {
+                database.AddNewUser("Tim", "Password");
+            }
+            catch (System.Data.SQLite.SQLiteException e)
+            {
+                Console.WriteLine("Username already exists.");
+            }
+
+            // Update Key Binding
+            Console.WriteLine(database.GetKeyBinding("Tim", Database.JUMP));
+            database.SetKeyBinding("Tim", Database.JUMP, "z");
+            Console.WriteLine(database.GetKeyBinding("Tim", Database.JUMP));
+
+            // Update Password
+            Console.WriteLine(database.GetPassword("PasswordIsTACO"));
+            database.SetPassword("PasswordIsTACO", "NotTACO");
+            Console.WriteLine(database.GetPassword("PasswordIsTACO"));
+
+            // Remove User
+            database.RemoveUser("PasswordIsTACO");
+
+        }
+    }
+}
