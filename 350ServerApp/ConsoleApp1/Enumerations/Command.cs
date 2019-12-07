3dc164fb527f38ad5ca43ebe784df0f11c82bc74
John Smith <jsmith@styleslogistics.com>
2019-12-06 20:27:24 -0500

    changed the database structure and added support for key mappings

---
+++ 350ServerApp/ConsoleApp1/Enumerations/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer.Enumerations
+{
+    public enum Command
+    {
+        IDLE = 0,
+        MOVING_LEFT = 1,
+        LOW_KICK = 2,
+        HIGH_KICK = 4,
+        PUNCH = 8,
+        BLOCKING = 16,
+        JUMPING = 32,
+        CROUCHING = 64,
+        MOVING_RIGHT = 128
+    }
+
+    public static class CommandUtilities
+    {
+        public static Command ParseCommand(byte commadByte)
+        {
+            return (Command)Enum.Parse(typeof(Command), Enum.GetName(typeof(Command), commadByte));
+        }
+    }
+}
